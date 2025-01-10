1c931eb701b91057ace83f6ee1ed1a12634f4911
Tyler Bauman <t.bauman@vehikl.com>
2025-01-10 15:49:20 -0500

    add queries

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,8 +1,8 @@
 import { gql } from "@apollo/client";
 
 export const GET_WORKOUTS = gql`
-  query GetWorkouts {
-    workouts {
+  query GetWorkouts($cycleId: Int!) {
+    workouts(where: { cycle: { _eq: $cycleId } }, order_by: { subtitle: asc }) {
       id
       title
       subtitle
@@ -12,6 +12,9 @@ export const GET_WORKOUTS = gql`
       description
       date
       cycle
+      workout_items(limit: 1, order_by: { created_at: asc }) {
+        notes
+      }
     }
   }
 `;
@@ -61,6 +64,61 @@ export const GET_EXERCISES = gql`
       demo_video_title
       demo_video_poster
       demo_video_id
+    }
+  }
+`;
+
+export const GET_PROGRAMS = gql`
+  query GetPrograms {
+    programs(order_by: [{ name: asc }]) {
+      id
+      name
+      description
+      image
+    }
+  }
+`;
+
+export const GET_USER = gql`
+  query GetUser($id: bigint!) {
+    users_by_pk(id: $id) {
+      id
+      email
+      image_url
+      created_at
+      updated_at
+      user_cycles(order_by: { created_at: desc }) {
+        id
+        cycle_id
+        current_workout
+        start_date
+        completed
+        created_at
+        updated_at
+        cycle {
+          id
+          cycle_number
+          date
+          bridge_week
+          program {
+            id
+            name
+            description
+            image
+          }
+          workouts(order_by: { subtitle: asc }) {
+            id
+            title
+            subtitle
+            poster
+            isRestDay
+            isActiveRecovery
+            description
+            date
+            cycle
+          }
+        }
+      }
     }
   }
 `;
